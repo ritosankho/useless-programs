5c930d280c71c60ab03077a18c1c1e1a3f68c844
rito <ritosankho@proton.me>
2025-10-14 18:01:29 +0530

    First commit

---
+++ area_of_regular_dodecagon.py
@@ -0,0 +1,10 @@
+import math
+
+s = int(input("Enter the length of the side of the Regular Dodecagon"))
+
+r3 = math.sqrt(3)
+
+area = 3*(2+r3)*s**2
+
+print("The area of your regular dodecagon is ", area)
+
